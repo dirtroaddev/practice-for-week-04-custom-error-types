5e61dd9eaef6e153a10667b83e9a0ddf5d9c173c
dirtroaddev <rongoodwindev@gmail.com>
2024-02-17 22:11:12 -0500

    Finished building out the classes

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -3,6 +3,18 @@
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
+class ValidationError extends Error {
+  constructor(message = "Invalid input", ...parameters) {
+    super(...parameters);
+    this.message = message;
+    this.name = "ValidationError";
+
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+  }
+}
+
 try {
   module.exports = ValidationError;
 } catch {
